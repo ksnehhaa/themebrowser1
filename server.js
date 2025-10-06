643d0c98fc59f5a27b215b3ffac26330f31c0d10
Kumari Sneha <ksnehaswn@gmail.com>
2025-10-07 01:08:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://theme-browser.vercel.app'],
+  origin: ['http://localhost:5173', 'https://themebrowser1.vercel.app'],
   credentials: true
 }));
 
